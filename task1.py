d989bb137fe466ef705a89c2e009a2e0117b9fbc
brittanylindberg98 <70421454+brittanylindberg98@users.noreply.github.com>
2020-10-30 23:25:43 -0700

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,7 @@
+#Write a program that calculates the number of students and average score
+a=int(input("Enter the score for Alice:"))
+b=int(input("Enter the score for Bob:"))
+c=int(input("Enter the score for Cindy:"))
+average=(a+b+c)/3
+print("The class average is",average,"for 3 students")
+
